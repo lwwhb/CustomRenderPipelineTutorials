d534e35e7dee28a5a2e50ad7927aebdf893a6d8c
haibo.wang <haibo.wang@unity.cn>
2024-01-19 12:25:09 +0800

    Lesson1 SetupLiteRP OK

---
+++ Assets/LiteRP/Runtime/LiteRPAsset.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace LiteRP
+{
+    [CreateAssetMenu(menuName = "Lite Render Pipeline/Lite Render Pipeline Asset")]
+    public class LiteRPAsset : RenderPipelineAsset<LiteRenderPipeline>
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new LiteRenderPipeline();
+        }
+    }
+}
---
+++ Assets/LiteRP/Runtime/LiteRenderPipeline.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace LiteRP
+{
+    public class LiteRenderPipeline : RenderPipeline
+    {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            
+        }
+    }
+}
