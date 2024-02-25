2ed6bc4f048c8cb6beaa4d977368e8d6cf416d0c
haibo.wang <haibo.wang@unity.cn>
2024-02-26 01:05:33 +0800

    Lesson 3 ok

--- Assets/LiteRP/Runtime/LiteRenderPipeline.cs
+++ Assets/LiteRP/Runtime/LiteRenderPipeline.cs
@@ -6,6 +6,7 @@ namespace LiteRP
 {
     public class LiteRenderPipeline : RenderPipeline
     {
+        private static readonly ShaderTagId s_ShaderTagId = new ShaderTagId("SRPDefaultUnlit");
         //老版本
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
@@ -42,12 +43,47 @@ namespace LiteRP
             CommandBuffer cmd = CommandBufferPool.Get(camera.name);
             //设置相机属性参数
             context.SetupCameraProperties(camera);
+
+            var clearFlags = camera.clearFlags;
+            bool clearSkybox = clearFlags == CameraClearFlags.Skybox;
+            bool clearDepth = clearFlags != CameraClearFlags.Nothing;
+            bool clearColor = clearFlags == CameraClearFlags.Color;
             //清理渲染目标
+            cmd.ClearRenderTarget(clearDepth, clearColor, CoreUtils.ConvertSRGBToActiveColorSpace(camera.backgroundColor));
+            
+            if (clearSkybox)
+            {
+                //绘制天空盒
+                var skyboxRendererList = context.CreateSkyboxRendererList(camera);
+                cmd.DrawRendererList(skyboxRendererList);
+            }
+
             //指定渲染排序设置SortSettings
+            var sortSettings = new SortingSettings(camera);
             //指定渲染状态设置DrawSettings
+            var drawSettings = new DrawingSettings(s_ShaderTagId, sortSettings);
+            
+            //绘制不透明物体
+            sortSettings.criteria = SortingCriteria.CommonOpaque;
             //指定渲染过滤设置FilterSettings
+            var filterSettings = new FilteringSettings(RenderQueueRange.opaque);
             //创建渲染列表
+            var rendererListParams = new RendererListParams(cullingResults, drawSettings, filterSettings);
+            var rendererList = context.CreateRendererList(ref rendererListParams);
             //绘制渲染列表
+            cmd.DrawRendererList(rendererList);
+            
+            //绘制半透明物体
+            sortSettings.criteria = SortingCriteria.CommonTransparent;
+            //指定渲染过滤设置FilterSettings
+            filterSettings = new FilteringSettings(RenderQueueRange.transparent);
+            //创建渲染列表
+            rendererListParams = new RendererListParams(cullingResults, drawSettings, filterSettings);
+            rendererList = context.CreateRendererList(ref rendererListParams);
+            //绘制渲染列表
+            cmd.DrawRendererList(rendererList);
+            
+            
             //提交命令缓冲区
             context.ExecuteCommandBuffer(cmd);
             //释放命令缓冲区
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CustomRenderPipelineTutorials
 Unity6的自定义渲染管线教程
 
-2月18日 Lesson1第一节 工程初始化与管线资源 代码已更新
-2月20日 Lesson2第二节 渲染管线基础流程 代码已更新
+* 2月18日 Lesson1第一节 工程初始化与管线资源 代码已更新
+* 2月20日 Lesson2第二节 渲染管线基础流程 代码已更新
+* 2月26日 Lesson2第三节 可见对象的渲染 代码已更新
